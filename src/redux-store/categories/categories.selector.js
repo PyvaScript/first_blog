d7bca350721a0c50e768be7546e1b0a955089363
Jason Bourne <json@treadstone.com>
2023-08-28 06:18:09 +1000

    Refactored categories to include reselect library

--- src/redux-store/categories/categories.selector.js
+++ src/redux-store/categories/categories.selector.js
@@ -1,4 +1,22 @@
-export const selectCategoriesMap=(state)=>state.categories.categoriesArray
-.reduce((acc, category)=>{const { title, items }=category; acc[ title ]=items; return acc},{});
+import { createSelector } from 'reselect';
+
+const selectCategoriesMapReducer=(state)=>state.categories;
+
+export const selectCategories=createSelector(
+    [ selectCategoriesMapReducer ],
+    (categories)=>categories.categoriesArray
+);
+
+export const selectCategoriesMap=createSelector(
+    [ selectCategories ],
+    (categories)=>categories.reduce((acc, category)=>{
+        const { title, items }=category;
+        acc[title]=items;
+        return acc;
+    }, {})
+);
+
+// export const selectCategoriesMap=(state)=>state.categories.categoriesArray
+// .reduce((acc, category)=>{const { title, items }=category; acc[ title ]=items; return acc},{});
 
 // export const selectCategoriesMap=(state)=>state.categories.categories;
